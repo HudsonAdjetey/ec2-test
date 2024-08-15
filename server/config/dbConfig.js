bee4fe988fb2c29e4dfa45305507975a25101d55
HudsonAdjetey <adjetey545@gmail.com>
2024-08-15 16:38:14 -0700

    removed node

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -9,9 +9,9 @@ const connDB = async () => {
     const conn = await mongoose.connect(URL, {
       // useNewUrlParser: true,
     });
-    if (process.env.NODE_ENV == "development") {
-      console.log(`MongoDB connected: ${conn.connection.host}`);
-    }
+    // if (process.env.NODE_ENV == "development") {
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+    // }
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
